73ecfb2265a4f2d17f750af8321b2a45b7828686
Samir Shrestha <theonlysamir@gmail.com>
2021-05-30 17:51:42 +0545

    Navbar updated

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,31 +1,47 @@
 import { Menu, Layout, Typography } from "antd";
+import {useRouter} from 'next/router'
 import { BulbFilled } from "@ant-design/icons";
 import Link from "next/link";
 const { Text } = Typography;
 
 export default function Navbar() {
+  const router = useRouter();
+
+  const routes = [
+    {
+      title: "Home",
+      href: "/",
+      key: "1"
+    },
+    {
+      title: "About Me",
+      href: "/about",
+      key: "2"
+    },
+    {
+      title: "Contact",
+      href: "/contact",
+      key: "3"
+    }
+  ]
   return (
     <Layout.Header className="d-flex align-items-center">
-      <div className="logo d-flex  align-items-center">
+      <div className="logo d-flex  align-items-center" onClick={()=>router.push('/')}>
         <BulbFilled className="text-white fs-5 px-1" />
         <Text className="text-white">thefakedeal</Text>
       </div>
-      <Menu mode="horizontal"  className="d-flex flex-wrap justify-content-end flex-grow-1" theme="dark">
-      <Menu.Item >
-          <Link href="/" key="1">
-            <a className="text-decoration-none ">Home</a>
-          </Link>
-        </Menu.Item>
-        <Menu.Item >
-          <Link href="/about" key="2">
-            <a className="text-decoration-none ">About Me</a>
-          </Link>
-        </Menu.Item>
-        <Menu.Item >
-          <Link href="/contact" key="3">
-            <a className="text-decoration-none ">Contact</a>
-          </Link>
-        </Menu.Item>
+      <Menu mode="horizontal" selectable={false} className="d-flex flex-wrap justify-content-end flex-grow-1" theme="dark">
+        {
+          routes.map(({href,title,key})=>(
+            <Menu.Item  >
+            <Link href={href} key={key}>
+              <a className="text-decoration-none ">
+                {title}
+              </a>
+            </Link>
+          </Menu.Item>
+          ))
+        }
       </Menu>
     </Layout.Header>
   );
