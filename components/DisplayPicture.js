57db9b9282d9c1c7d840d7562239ecdc638edb88
Samir Shrestha <theonlysamir@gmail.com>
2025-03-13 23:05:06 +0545

    clear all

--- components/DisplayPicture.js
+++
@@ -1,8 +0,0 @@
-import styles from  '../styles/DisplayPic.module.css';
-export default function DisplayPicture() {
-  return (
-    <div className={styles.outside}>
-        <div className={styles.container}></div>
-    </div>
-  )
-}
