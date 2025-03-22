63337a1ed2ae5c655437ec878f2928db07860e6a
Samir Shrestha <theonlysamir@gmail.com>
2025-03-23 03:05:54 +0545

    icon

--- Component/ActiveLink.tsx
+++ Component/ActiveLink.tsx
@@ -1,19 +1,25 @@
 "use client";
 import Link from "next/link"
 import { usePathname } from "next/navigation"
+import React, { ElementType } from "react";
 
 
 type Props = {
     title: string
     href: string,
-
+    icon?: React.ReactNode
 }
 
-export default function ActiveLink({title, href}: Props) {
+export default function ActiveLink({title, href, icon: Icon}: Props) {
   const pathname = usePathname();
   return (
     <Link href={href} className={`font-bold hover:underline hover:decoration-accent hover:decoration-dotted
         ${(pathname === href) ? "text-text/90":"text-text/60"}
-    `}><li>{title}</li></Link>
+    `}
+    about={title}
+    title={title}
+    ><li className="flex items-center justify-center gap-2 "> 
+      {Icon && Icon}
+      <span className="hidden md:block">{title}</span></li></Link>
   )
 }
