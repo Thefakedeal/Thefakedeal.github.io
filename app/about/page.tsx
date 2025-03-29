e4443e042ea69a649f22390e14aa52790b3308e2
Samir Shrestha <theonlysamir@gmail.com>
2025-03-30 02:26:45 +0545

    meta data

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,7 +6,40 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Samir Shrestha | About",
-  description: "Samir Shrestha's Portfolio",
+  description: "I'm Samir Shrestha, a software developer with a strong focus on web technologies like Laravel, React, Next.js, and Vue.js. With 4+ years of experience, I specialize in building scalable applications and optimizing performance. Beyond coding, I have a deep passion for strength training, fitness coaching, and crafting high-protein, budget-friendly meal plans. Explore my projects, insights, and blogs as I blend tech, fitness, and problem-solving into my daily life.",
+  openGraph: {
+    title: "Samir Shrestha | About",
+    siteName: "samirshrestha.info.np",
+    type: "website",
+    description:"I'm Samir Shrestha, a software developer with a strong focus on web technologies like Laravel, React, Next.js, and Vue.js. With 4+ years of experience, I specialize in building scalable applications and optimizing performance. Beyond coding, I have a deep passion for strength training, fitness coaching, and crafting high-protein, budget-friendly meal plans. Explore my projects, insights, and blogs as I blend tech, fitness, and problem-solving into my daily life.",
+    images: [
+        {
+            url: "https://samirshrestha.info.np/images/avatar.webp"
+        },
+    ],
+    },
+
+    twitter: {
+        title: "Samir Shrestha | About",
+        description: "I'm Samir Shrestha, a software developer with a strong focus on web technologies like Laravel, React, Next.js, and Vue.js. With 4+ years of experience, I specialize in building scalable applications and optimizing performance. Beyond coding, I have a deep passion for strength training, fitness coaching, and crafting high-protein, budget-friendly meal plans. Explore my projects, insights, and blogs as I blend tech, fitness, and problem-solving into my daily life.",
+        card: "summary_large_image",
+        site: "samirshrestha.info.np",
+        images: [
+          {
+              url: "https://samirshrestha.info.np/images.avatar.webp"
+          },
+        ],
+    },
+
+    robots: {
+        index: true,
+        follow: true,
+        nocache: true,
+        googleBot: {
+            index: true,
+            follow: true,
+        },
+},
 };
 
 
