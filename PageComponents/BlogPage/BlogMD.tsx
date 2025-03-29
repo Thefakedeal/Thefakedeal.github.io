20444b7377399477b61b2a214a56034abdbc20c5
Samir Shrestha <theonlysamir@gmail.com>
2025-03-29 18:46:05 +0545

    date and audit

--- PageComponents/BlogPage/BlogMD.tsx
+++ PageComponents/BlogPage/BlogMD.tsx
@@ -1,33 +1,55 @@
-"use client"
-import { MDXRemote } from 'next-mdx-remote'
-import { serialize } from 'next-mdx-remote/serialize'
-import {  useLayoutEffect, useState } from 'react'
+// "use client"
+import { MDXRemote } from 'next-mdx-remote/rsc'
+import Image from 'next/image'
+import Link from 'next/link'
+import { Suspense } from 'react'
+// import { serialize } from 'next-mdx-remote/serialize'
+// import {  useLayoutEffect, useState } from 'react'
 type Props = {
     content: string
 }
 
-export const BlogMD =  ({content}: Props) => {
-  const [loading, setLoading] = useState(true);
-  const [mdxSource, setMdxSource] = useState<any>(null);
-  const getMdxSource = async () => {
-    setLoading(true);
-    const mdx = await serialize(content);
-    setMdxSource(mdx);
-    setLoading(false);
-  }
+const components = {
+  p : (props: any) => <p className='text-text my-2'>{props.children}</p>,
+  a : (props:{children: string, href: string}) => <Link href={props.href} className='text-accent font-bold'>{props.children}</Link>,
+  img: (props:{src: string, alt: string}) => <Image 
+   src={props.src} alt={props.alt} className='w-full p-1'
+   width={400} height={300}
+   />
+}
 
-  useLayoutEffect(() => {
-    getMdxSource();  
-  },[]);
+export const BlogMD =  ({content}: Props) => {
+  // const [loading, setLoading] = useState(true);
+  // const [mdxSource, setMdxSource] = useState<any>(null);
+  // const getMdxSource = async () => {
+  //   setLoading(true);
+  //   const mdx = await serialize(content);
+  //   setMdxSource(mdx);
+  //   setLoading(false);
+  // }
+
+  // useLayoutEffect(() => {
+  //   getMdxSource();  
+  // },[]);
+
   return (
     <section className="flex flex-col align-center justify-center">
          {
-            (loading || !mdxSource)?(
-                <span className="loading loading-bars loading-xl"></span>
-            ):(
-                <MDXRemote {...mdxSource} />
-            )
+          <Suspense fallback={<span className="loading loading-bars loading-xl"></span>}>
+            <MDXRemote  components={components} source={content} />
+          </Suspense>
          }   
     </section>
   )
+  // return (
+  //   <section className="flex flex-col align-center justify-center">
+  //        {
+  //           (loading || !mdxSource)?(
+  //               <span className="loading loading-bars loading-xl"></span>
+  //           ):(
+  //               <MDXRemote {...mdxSource} />
+  //           )
+  //        }   
+  //   </section>
+  // )
 }
--- app/blogs/[slug]/page.tsx
+++ app/blogs/[slug]/page.tsx
@@ -2,7 +2,7 @@ import { getPostBySlug } from '@/helper/blog'
 import { BlogMD } from '@/PageComponents/BlogPage/BlogMD'
 import Image from 'next/image'
 import { notFound } from 'next/navigation'
-
+import { formatDistance} from 'date-fns'
 
 type Props = {
     params: Promise<{
@@ -21,9 +21,11 @@ async function page({params}: Props) {
     <main className="w-screen flex flex-col items-center">
         <article className="glass w-full md:w-3/4 p-4 mb-5 flex flex-col gap-2 min-h-[80vh]">
           <h2>{data.title}</h2>
+          <span className='text-sm mx-1 text-text/60'>Last Update: {data.date} {" "}
+            ({formatDistance(new Date(data.date), new Date(), { addSuffix: true })})</span>
           {
             data.image && (
-              <Image src={data.image} alt={data.title} width={500} height={500} 
+              <Image src={data.image} alt={data.title} width={400} height={300} 
                 className='w-full p-5'
               />
             )
